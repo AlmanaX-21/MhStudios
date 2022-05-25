d2cc87d9f9bbe57950add474f6c7aa6d7b650906
AlmanaX-21 <captkanishqsihag@gmail.com>
2022-05-25 13:18:47 +0530

    Initial commit

---
+++ src/main/java/me/almana/mhstudios/MhStudios.java
@@ -0,0 +1,37 @@
+package me.almana.mhstudios;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "MhStudios",
+        name = "MhStudios",
+        version = "1.0",
+        description = "Basic rank forwarding for Minehut Studios server",
+        authors = {"AlmanaX21"}
+)
+public class MhStudios {
+
+    public Logger logger;
+    public ProxyServer server;
+
+    @Inject
+    public void pluginInitialise(ProxyServer server, Logger logger) {
+
+        this.server = server;
+        this.logger = logger;
+
+        logger.info("Plugin constructed.");
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event, Logger logger) {
+
+        this.logger = logger;
+        logger.info("Plugin initialised");
+    }
+}
